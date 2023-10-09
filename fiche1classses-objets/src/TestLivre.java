26309442ab1de093643fca3e042796669580d18c
yukydesu <kylliandelbauchep@gmail.com>
2023-10-09 18:42:30 +0200

    c

--- fiche1classses-objets/src/TestLivre.java
+++ fiche1classses-objets/src/TestLivre.java
@@ -4,6 +4,6 @@ public class TestLivre {
 
         Livre livre1 =  new Livre("9782910188139", "L'Alchimist", "Coelho Paulo"
         , 10.50, 1988, 255);
-
+        System.out.println(livre1);
     }
 }
